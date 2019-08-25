f47552bad15fd0ed88f407f39c0703f2cf04c54f
Michael Omoto <omoto.mike@gmail.com>
2019-08-25 12:23:07 +0900

    removing > django110 support

--- localeurl/urls.py
+++ localeurl/urls.py
@@ -1,9 +1,8 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
 from localeurl.views import change_locale
 
-urlpatterns = patterns('',
-	url(r'^change/', change_locale, name='localeurl_change_locale'),
+
+from django.conf.urls import url
+
+urlpatterns = (
+    url(r'^change/', change_locale, name='localeurl_change_locale'),
 )
