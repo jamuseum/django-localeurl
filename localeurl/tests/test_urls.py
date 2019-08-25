f90436a5a6515fb58b5b2e6e3efc762ae0dea379
Michael Omoto <omoto.mike@gmail.com>
2019-08-25 12:26:13 +0900

    fixing test urls dummy

--- localeurl/tests/test_urls.py
+++ localeurl/tests/test_urls.py
@@ -1,15 +1,16 @@
 """
 URLconf for testing.
 """
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('localeurl.tests.test_urls',
-     url(r'^dummy/$', 'dummy', name='dummy0'),
-     url(r'^dummy/(?P<test>.+)$', 'dummy', name='dummy1'),
-)
+from django.conf.urls import url
+
 
 def dummy(request, test='test'):
     pass
+
+
+urlpatterns = [
+     url(r'^dummy/$', dummy, name='dummy0'),
+     url(r'^dummy/(?P<test>.+)$', dummy, name='dummy1'),
+]
+
