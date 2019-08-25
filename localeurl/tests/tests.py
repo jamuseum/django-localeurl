957c47bca77183a25d5e4ca4dc7d4e203d4a4051
Michael Omoto <omoto.mike@gmail.com>
2019-08-25 13:37:29 +0900

    adding tests for redirect path

--- localeurl/tests/tests.py
+++ localeurl/tests/tests.py
@@ -474,9 +474,9 @@ class ViewsTestCase(LocaleurlTestCase):
     def test_change_without_next(self):
         response = self.client.post('/change/', data={'locale': 'de'})
         self.assertEqual(response.status_code, 302)
-        # assert redirect path == '/'
+        self.assertEqual(response.url, '/de/')
 
     def test_change_with_next(self):
         response = self.client.post('/change/', data={'locale': 'de', 'next': '/foo'})
         self.assertEqual(response.status_code, 302)
-        # assert redirect path == '/foo'
+        self.assertEqual(response.url, '/de/foo')
